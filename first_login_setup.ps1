e209bf663b1d8637ef2194b6a7087946269e31a1
kaeser-sosae <kaeser-sosae@users.noreply.github.com>
2024-10-01 12:20:07 +0900

    Initial commit via upload

---
+++ first_login_setup.ps1
@@ -0,0 +1,15 @@
+# Show the interface
+
+# Remove software that we don't want
+
+# Install software that we do want
+
+# Disable System Protection on C: drive
+$Drive = "C:"
+Disable-ComputerRestore -Drive $Drive
+
+# Set up power plan
+
+
+
+
